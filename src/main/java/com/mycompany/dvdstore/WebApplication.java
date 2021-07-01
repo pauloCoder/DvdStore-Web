31003491699dce793c0f3428f839048cc61fcecc
PauloCoder <williams56@hotmail.fr>
2021-07-02 01:20:37 +0200

    Déproxification de l'acteur principal et des avis du film (Web)

--- src/main/java/com/mycompany/dvdstore/WebApplication.java
+++ src/main/java/com/mycompany/dvdstore/WebApplication.java
@@ -2,12 +2,21 @@ package com.mycompany.dvdstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 
 @SpringBootApplication
 public class WebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
+	}
+	
+	@Bean
+	Hibernate5Module datatypeHibernateModule()
+	{
+		return new Hibernate5Module();
 	}
 
 }
